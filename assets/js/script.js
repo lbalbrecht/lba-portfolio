4a12995014753b77cfd32bc56daa0e442dde8578
lbalbrecht <lalbrecht18@wooster.edu>
2021-05-22 22:54:11 -0700

    linked github repositories

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+$("#gearwise-btn").on('click', function(e) {
+    e.preventDefault();
+    window.open('https://github.com/tylercmac/gear-tracker', '_blank');
+});
+
+$("#day-planner-btn").on('click', function(e) {
+    e.preventDefault();
+    window.open('https://github.com/lbalbrecht/lba-day-planner', '_blank');
+});
+
+$("#pass-gen-btn").on('click', function(e) {
+    e.preventDefault();
+    window.open('https://github.com/lbalbrecht/lba-password-generator', '_blank');
+});
+
+$("#weather-dash-btn").on('click', function(e) {
+    e.preventDefault();
+    window.open('https://github.com/lbalbrecht/lba-weather-dashboard', '_blank');
+});
+
+$("#code-quiz-btn").on('click', function(e) {
+    e.preventDefault();
+    window.open('https://github.com/lbalbrecht/lba-coding-quiz', '_blank');
+});
