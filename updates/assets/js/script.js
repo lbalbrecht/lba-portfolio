d0b37396e730bd1f759c96a003be1854c48dc8a1
lbalbrecht <lalbrecht18@wooster.edu>
2021-09-16 19:12:09 -0700

    updated styling

---
+++ updates/assets/js/script.js
@@ -0,0 +1,39 @@
+$("#resume-link-btn").on('click', function(e) {
+    e.preventDefault();
+    window.open('https://drive.google.com/file/d/1ndsYEZuGY0PVlRDGKSkNy-EjrkXwl4Ww/view?usp=sharing', '_blank');
+});
+
+$("#gearwise-btn").on('click', function(e) {
+    e.preventDefault();
+    window.open('https://github.com/tylercmac/gear-tracker', '_blank');
+});
+
+$("#day-planner-btn").on('click', function(e) {
+    e.preventDefault();
+    window.open('https://github.com/lbalbrecht/lba-day-planner', '_blank');
+});
+
+$("#pass-gen-btn").on('click', function(e) {
+    e.preventDefault();
+    window.open('https://github.com/lbalbrecht/lba-password-generator', '_blank');
+});
+
+$("#weather-dash-btn").on('click', function(e) {
+    e.preventDefault();
+    window.open('https://github.com/lbalbrecht/lba-weather-dashboard', '_blank');
+});
+
+$("#readme-generator-btn").on('click', function(e) {
+    e.preventDefault();
+    window.open('https://github.com/lbalbrecht/lba-readme-generator', '_blank');
+});
+
+$("#team-profile-btn").on('click', function(e) {
+    e.preventDefault();
+    window.open('https://github.com/lbalbrecht/lba-team-profile-generator', '_blank');
+});
+
+$("#note-taker-btn").on('click', function(e) {
+    e.preventDefault();
+    window.open('https://github.com/lbalbrecht/lba-note-taker', '_blank');
+});
